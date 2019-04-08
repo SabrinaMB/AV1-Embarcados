1d34b360d3bfa230759a0139ea95119e8aa14a71
SabrinaMB <sbrnmchd@gmail.com>
2019-04-08 15:42:44 -0300

    printando valores na tela

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -12,6 +12,13 @@
 #include "arial_72.h"
 
 
+
+
+#define BUT_PIO_VR			PIOA
+#define BUT_PIO_ID_VR		19
+#define BUT_PIO_IDX_VR		2u
+#define BUT_PIO_IDX_MASK_VR (1u << BUT_PIO_IDX_VR)
+
 struct ili9488_opt_t g_ili9488_display_opt;
 
 void configure_lcd(void){
@@ -24,6 +31,29 @@ void configure_lcd(void){
 	/* Initialize LCD */
 	ili9488_init(&g_ili9488_display_opt);
 	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
+	
+}
+
+void init(void){
+	
+	pmc_enable_periph_clk(BUT_PIO_ID_VR);
+
+	pio_configure(BUT_PIO_VR, PIO_INPUT, BUT_PIO_IDX_MASK_VR, PIO_PULLUP);
+	
+	pio_handler_set(BUT_PIO_VR,
+	BUT_PIO_ID_VR,
+	BUT_PIO_IDX_MASK_VR,
+	PIO_IT_FALL_EDGE,
+	but_callback);
+	
+	// Ativa interrupção
+	pio_enable_interrupt(BUT_PIO_VR, BUT_PIO_IDX_MASK_VR);
+
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
+	NVIC_EnableIRQ(BUT_PIO_ID_VR);
+	NVIC_SetPriority(BUT_PIO_ID_VR, 0); // Prioridade 4
+
 	
 }
 
@@ -42,15 +72,27 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 	}	
 }
 
+//float velocidade(int pulsos, float tempo){
+//	return angular*0,65;
+//}
+
+//float distancia(int pulsos){
+//	return 2*PI*angular*tempo;
+//}
+
 
 int main(void) {
 	board_init();
 	sysclk_init();	
 	configure_lcd();
 	
-	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
-	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
-	font_draw_text(&arial_72, "102456", 50, 200, 2);
+	char buffer[32];
+	int x = 12;
+	sprintf(buffer, "pessoal!! %d", x);
+	
+	font_draw_text(&sourcecodepro_28, "oimundo", 50, 50, 1);
+	font_draw_text(&calibri_36, buffer, 50, 100, 1);
+	//font_draw_text(&arial_72, sprintf(buffer, "%d", x), 50, 200, 2);
 	while(1) {
 		
 	}
